409f7b456da8ca7cdff31b890393a0a600793d6c
Egidio Caprino <egidio.caprino@socialmediaemotions.com>
2019-03-29 10:54:42 +0100

    chore: implement assignement 2

---
+++ assignement_2/lib/product_form.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProductForm extends StatelessWidget {
+  
+  @override Widget build(BuildContext context) {
+    return Text('Product Form');
+  }
+  
+}
